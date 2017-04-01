cc8aff6c05d6e08fd47d43c335f611a35a051c33
郭飞飞 <myEmail@example.com>
2017-04-01 09:20:42 +0800

    创建工程

---
+++ SelectImage/SelectImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectImage
+//
+//  Created by ZZCN77 on 2017/4/1.
+//  Copyright © 2017年 ZZCN77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SelectImage/SelectImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SelectImage
+//
+//  Created by ZZCN77 on 2017/4/1.
+//  Copyright © 2017年 ZZCN77. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
